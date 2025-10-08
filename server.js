cd4b29764b8c8e38fba8484d12ad29fea3ef046a
vma225147 <vma225147@gmail.com>
2025-10-08 18:38:55 +0000

    Update server.js

--- server.js
+++ server.js
@@ -28,11 +28,7 @@ app.use((req, res) => {
   res.status(404).render('pages/home', { title: 'Not Found' });
 });
 
-// 500 - Internal Server Error
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).render('pages/error', { title: 'Server Error' });
-});
+
 
 // Start server
 const PORT = process.env.PORT || 3000;
