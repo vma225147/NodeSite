3706f5910d274bc0f69885b394d81fc5ecfe7f51
vma225147 <vma225147@gmail.com>
2025-10-08 18:41:01 +0000

    fixing

---
+++ views/pages/error.js
@@ -0,0 +1,7 @@
+<%- include('../layout', { body: `
+  <section class="error">
+    <h1>500 - Server Error</h1>
+    <p>Something went wrong on our end. Please try again later.</p>
+  </section>
+`, title: title }) %>
+
